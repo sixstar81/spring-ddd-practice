b1ad2ef66b0322bd7dcf39ac3aa815b015d4f401
이상국 <sixstar6@dbgroup.co.kr>
2024-11-18 08:22:47 +0900

    order domain entity created

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/sk/order/service/domain/entity/Customer.java
@@ -0,0 +1,9 @@
+package com.sk.order.service.domain.entity;
+
+import com.sk.domain.entity.AggregateRoot;
+import com.sk.domain.valueobject.CustomerId;
+
+import java.util.UUID;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
