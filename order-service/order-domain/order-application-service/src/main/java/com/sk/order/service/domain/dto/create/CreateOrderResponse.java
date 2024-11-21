427faa827f36221c971ce55f7a2b8f7a137f465e
이상국 <sixstar6@dbgroup.co.kr>
2024-11-22 07:59:01 +0900

    order domain dto created

---
+++ order-service/order-domain/order-application-service/src/main/java/com/sk/order/service/domain/dto/create/CreateOrderResponse.java
@@ -0,0 +1,22 @@
+package com.sk.order.service.domain.dto.create;
+
+import com.sk.domain.valueobject.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderResponse {
+
+    @NotNull
+    private final UUID orderTrackingId;
+    @NotNull
+    private final OrderStatus orderStatus;
+    @NotNull
+    private final String message;
+}
