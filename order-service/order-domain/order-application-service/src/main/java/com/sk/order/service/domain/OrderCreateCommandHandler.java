3acd82e594e99e7e4a47df3f2e838849ee679f55
이상국 <sixstar6@dbgroup.co.kr>
2024-11-25 08:54:44 +0900

    OrderCreateCommandHandler createOrder

--- order-service/order-domain/order-application-service/src/main/java/com/sk/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/sk/order/service/domain/OrderCreateCommandHandler.java
@@ -2,13 +2,53 @@ package com.sk.order.service.domain;
 
 import com.sk.order.service.domain.dto.create.CreateOrderCommand;
 import com.sk.order.service.domain.dto.create.CreateOrderResponse;
+import com.sk.order.service.domain.entity.Customer;
+import com.sk.order.service.domain.exception.OrderDomainException;
+import com.sk.order.service.domain.mapper.OrderDataMapper;
+import com.sk.order.service.domain.port.output.repository.CustomerRepository;
+import com.sk.order.service.domain.port.output.repository.OrderRepository;
+import com.sk.order.service.domain.port.output.repository.RestaurantRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Component
 public class OrderCreateCommandHandler {
+
+    //--* Application Service 에서 Domain Service 를 호출해야 한다.
+    private final OrderDomainService orderDomainService;
+    private final OrderRepository orderRepository;
+    private final CustomerRepository customerRepository;
+    private final RestaurantRepository restaurantRepository;
+
+    private final OrderDataMapper orderDataMapper;
+
+    public OrderCreateCommandHandler(OrderDomainService orderDomainService,
+                                     OrderRepository orderRepository,
+                                     CustomerRepository customerRepository,
+                                     RestaurantRepository restaurantRepository,
+                                     OrderDataMapper orderDataMapper) {
+        this.orderDomainService = orderDomainService;
+        this.orderRepository = orderRepository;
+        this.customerRepository = customerRepository;
+        this.restaurantRepository = restaurantRepository;
+        this.orderDataMapper = orderDataMapper;
+    }
+
+    @Transactional
     public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand){
+        //1. 해당 고객이 있는지 여부를 확인한다.
+        checkCustomer(createOrderCommand.getCustomerId());
         return null;
+    }
+
+    private void checkCustomer(UUID customerId) {
+        Optional<Customer> customer = customerRepository.findCustomer(customerId);
+        if(customer.isEmpty())
+            throw new OrderDomainException("could not find customer with customer id :" + customerId);
     }
 }
